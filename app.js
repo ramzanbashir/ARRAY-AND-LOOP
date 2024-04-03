1839a01c54d7868b889208d91f389e56764c7c46
MUHAMMAD RAMZAN <153890643+ramzanbashir@users.noreply.github.com>
2024-04-03 17:06:53 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,168 +1,101 @@
-//----------------------Chapter no.14 - 15----------------------
 
-//---------------------------Q1---------------------------
-// var studentName = [];
-// studentName.push("Muhammad Ramzan");
-// console.log(studentName);
+// ______________________ CHAPTER#17-20__________________________
 
-//---------------------------Q2---------------------------
-// var studentName = [];
-// studentName.push("Muhammad Ramzan");
-// console.log(studentName);
+// --------------------------Task-1------------------------------
+// var emptyArray = [[]];
+// console.log(emptyArray)
 
-//---------------------------Q3---------------------------
-// var studentName = ["Ramzan","adeeb","shazaib","waleed","tamoor","Adnan"];
-// console.log(studentName);
+// --------------------------Task-2------------------------------
+// var multiplearry = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1],
+// ]
+// console.log(multiplearry);
 
-//---------------------------Q4---------------------------
-// var num = [1,2,3,4,5];
-// console.log(num);
+// --------------------------Task-3------------------------------
+// for(var i=1; i<=10; i++){
+// document.write(`${i} <br>`)
+// }
 
-// //---------------------------Q5---------------------------
-// var booleanArray = [true, false];
-// console.log(booleanArray);
+// --------------------------Task-4------------------------------
+// var tableNo = prompt("Enter Table No");
+// var tableLength = prompt("Enter Table Length");
+// for(var i=1; i<=tableLength; i++){
+//     document.write(`${tableNo} x ${i} = ${tableNo*i} <br>`)
+// }
 
-//---------------------------Q6---------------------------
-// var mixedArray = [1, "Muhammad Ramzan", true , false];
-// console.log(mixedArray);
+// --------------------------Task-5------------------------------
+// var fruits = ["Mango","Orange","Strawbery","Bluebery","Banana","Apple","Graps","Chiko","Falsa"]
+// for(var i=0; i<=fruits.length; i++){
+//     document.write(`${fruits[i]} <br>`)
+// }
+// for(var i=0; i<=fruits.length; i++){
+//     document.write(`Element at Index ${i} is ${fruits[i]} <br>`)
+// }
+// document.write("<br>")
 
-//---------------------------Q7---------------------------
-var qualification1 = ["SSC <br>"];
-var qualification2 = ["HSC <br>"];
-var qualification3 = ["BCS <br>"];
-var qualification4 = ["BS <br>"];
-var qualification5 = ["BCOM <br>"];
-var qualification6 = ["MS <br>"];
-var qualification7 = ["MS <br>"];
-var qualification8 = ["Phd <br>"];
+// --------------------------Task-6------------------------------
+// document.write("<b>A..COUNTING:</b>")
+// for(var i=1; i<=15; i++){
+//     document.write(i + ",");
+// }
+// document.write("<br>") 
 
-document.write(`
-<table >
-<tr>
-<td>
-<h1>Qualification:</h1>
- ${qualification1}
- ${qualification2}
- ${qualification3}
- ${qualification4}
- ${qualification5}
- ${qualification6}
- ${qualification7} 
- ${qualification8} 
+// document.write("<br><b>B..REVERSE COUNTING:</b>")
+// for(var i=15; i>=1; i--){
+//     document.write(i + ",");
+// }
+// document.write("<br>")
 
-</td>
- </tr>
- </table>
-`);
+// document.write("<br><b>C..EVEN NO:</b>")
+// for(var i=0; i<=20; i=i+2){
+//     document.write(i + ",");
+// }
 
-//---------------------------Q8---------------------------
-// var studentName = ["Usman", "Salman", "Adnan"];
-// var studentMarks = [320 , 230, 480];
-// var totalMarks = 500;
-// var studentPercent1 = studentMarks[0]/totalMarks*100;
-// var studentPercent2 = studentMarks[1]/totalMarks*100;
-// var studentPercent3 = studentMarks[2]/totalMarks*100;
+// document.write("<br>")
 
-// document.write(`
-//     Score of ${studentName[0]} is ${studentMarks[0]}. Percentage: ${studentPercent1}%<br>
-//     Score of ${studentName[1]} is ${studentMarks[1]}. Percentage: ${studentPercent2}%<br>
-//     Score of ${studentName[2]} is ${studentMarks[2]}. Percentage: ${studentPercent3}%
-// `)
+// document.write("<br><b>D..ODD NO :</b>")
+// for(var i=1; i<=20; i=i+2){
+//     document.write(i + ",");
+// }
 
-//---------------------------Q9---------------------------
-// document.write(`<h4>List of Colors in an Array</h4>`);
-// var colorList = ["Red","Blue","Green","Yellow","Orange"];
-// document.write(`${colorList}<br>`);
-// // //---------------------------Q9(a)---------------------------
-// var newColorStart = prompt("Write a color name to add in the start of an array","Golden");
-// colorList.unshift(newColorStart);
-// document.write(`<h4>New Color Added in Start of Array</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(b)---------------------------
-// var newColorEnd = prompt("Write a color name to add in the end of an array","Purple");
-// colorList.push(newColorEnd);
-// document.write(`<h4>New Color Added in the End of Array</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(c)---------------------------
-// var newColorStart1 = prompt("Write a color name to add in the 1st number of an array","Brown");
-// var newColorStart2 = prompt("Write a color name to add in the 2nd number of an array","Black");
-// colorList.unshift(newColorStart1,newColorStart2);
-// document.write(`<h4>New 2 Colors Added in the Start</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(d)---------------------------
-// colorList.shift(newColorStart1);
-// document.write(`<h4>No 1 Color Deleted</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(e)---------------------------
-// colorList.pop(newColorEnd);
-// document.write(`<h4>Last Color Deleted</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(f)---------------------------
-// var newColor = prompt("Write a color name to add" ,"Indigo");
-// var position = prompt("Enter a position where the color is add");
-// colorList.splice(position,0,newColor); 
-// document.write(`<h4>New Color Added on the given position</h4>${colorList}<br><br>`);
-// // //---------------------------Q9(g)---------------------------
-// var indexNo = prompt("Enter a index no you whant to delete color");
-// var colorRange = prompt("How many color do you want to delete");
-// colorList.splice(indexNo,colorRange); 
-// document.write(`<h4>Colors deleted on the given range</h4>${colorList}<br><br>`);
+// document.write("<br>")
 
+// document.write("<br><b>E..SERIES:</b>")
+// for(var i=1; i<=20; i=i+2){
+//     document.write(i + "K,");
+// }
 
+// --------------------------Task-7------------------------------
+// var products =["cake","apple pie","cookie","chips","patties","heart biskuts"]
+// var user = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?").toLowerCase()
+// var match = false 
 
-//---------------------------Q10---------------------------
-// var score1 = [320,280,480,120];
-// var score2 = [120,480,280.320];
-// score1.sort()
-// document.write(`
-//     Ordered Score of Students: ${score1}
-// `);
+// for(i=0; i<=products.length; i++){
+// if(user == products[i] ){
+// document.write(`${user} is found at index ${i}`);
+// match = true
+// break
+// }
+// }
+// if(!match){
+//     document.write(`we are sorry${user} is <b> not available <>in our bakery`);
+// }
+// document.write("<br>")
 
-//---------------------------Q11---------------------------
-// var cities = [`Karachi`,"Lahore","Islamabad","Quetta","Peshawar"];
-// var selectedCities = [];
-// selectedCities.push(...cities.slice(2,4));
-// console.log(selectedCities);
+// --------------------------Task-8------------------------------
+// var arrayNumber = [24,54,78,91,12]
+// var largestNumber = Math.max(...arrayNumber)
+// document.write(`<b>Array items:</b> ${arrayNumber}<br><br> <b> The Largest number is :</b> ${largestNumber}<br>`);
 
-//---------------------------Q12---------------------------
-// var arr = ["This ", " is ", " my ", " cat"];
-// var arrJoin = arr.join("")
-// document.write(`
-//     Array:<br>
-//     ${arr}<br>
-//     <br>
-//     String:<br>
-//     ${arrJoin}
-// `)
+// // --------------------------Task-9------------------------------
+// var arrayNumber = [24,54,78,91,12]
+// var smallestNumber = Math.min(...arrayNumber)
+// document.write(`<b>Array items:</b> ${arrayNumber}<br><br> <b> The Largest number is :</b> ${smallestNumber}<br>`);
 
-// //---------------------------Q13---------------------------
-// var devices = ["Keyboard","Mouse","Printer","Monitor"];
-// document.write(`
-//   <ul>
-//   <li>${devices[0]}</li>
-//   <li>${devices[1]}</li>
-//   <li>${devices[2]}</li>
-//   <li>${devices[3]}</li>
-//   </ul>
-// `)
-
-// //---------------------------Q14---------------------------
-// var devices = ["Keyboard","Mouse","Printer","Monitor"];
-// devices.reverse();
-// document.write(`
-//   <ul>
-//   <li>${devices[0]}</li>
-//   <li>${devices[1]}</li>
-//   <li>${devices[2]}</li>
-//   <li>${devices[3]}</li>
-//   </ul>
-// `);
-
-// //---------------------------Q15---------------------------
-// var phoneManufacture = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
-// document.write(`
-//   <label>Phone Manufactures</label>
-//   <select>
-//   <option>${phoneManufacture[0]}</option>
-//   <option>${phoneManufacture[1]}</option>
-//   <option>${phoneManufacture[2]}</option>
-//   <option>${phoneManufacture[3]}</option>
-//   <option>${phoneManufacture[4]}</option>
-//   <option>${phoneManufacture[5]}</option>
-//   </select>
-// `);
+// --------------------------Task-10------------------------------
+// var number = 5;
+// for(i=1; i<=20; i++){
+//     document.write(`<b> ${number*i} </b>,`)
+// }
